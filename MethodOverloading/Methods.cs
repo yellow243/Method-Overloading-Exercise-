e19c455caaff094125f503ed9ab634ab9b8e21c4
Zach Padovano <zacharyaz10@gmail.com>
2023-04-17 09:19:56 -0700

    First commit

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public static double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+        public static string Add(int number1, int number2, bool isCheck)
+        {
+            var sum = number1 + number2;
+
+            if (isCheck == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCheck == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (isCheck == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          var answer = Methods.Add(672, 154);
+            Console.WriteLine(answer);
+           var answer2 = Methods.Add(6.261, 9.811);
+            Console.WriteLine(answer2);
+            var answer3 = Methods.Add(153, 1047, true);
+            Console.WriteLine(answer3);
         }
     }
 }
